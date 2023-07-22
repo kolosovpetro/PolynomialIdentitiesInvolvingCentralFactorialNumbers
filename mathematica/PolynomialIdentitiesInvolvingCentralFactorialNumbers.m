af752f4501d1d05e696f47dd6e101f69d74d4276
kolosovpetro <kolosovp94@gmail.com>
2023-07-22 03:55:10 +0200

    identity in central factorial numbers

--- mathematica/PolynomialIdentitiesInvolvingCentralFactorialNumbers.m
+++ mathematica/PolynomialIdentitiesInvolvingCentralFactorialNumbers.m
@@ -5,6 +5,7 @@ BeginPackage["PolynomialIdentitiesInvolvingCentralFactorialNumbers`"]
 CentralFactorialNumber1::usage= "recursively defines central factorial numbers of the second kind (in the context of Knuth and Riordan https://arxiv.org/pdf/math/9207222.pdf)"
 CFNIdentity1::usage= "Defines an identity in central factorial numbers (2k-1)! T(2n,2k) = \\frac{1}{k} \\sum_{j=0}^{k} (-1)^j \\binom{2k}{j} (k-j)^{2n}"
 CFNIdentity2::usage= "Defines an identity in central factorial numbers (2k-1)! T(2n,2k) = \\frac{1}{k} \\sum_{j=0}^{k} (-1)^{k-j} \\binom{2k}{k-j} j^{2n}"
+CFNIdentity3::usage= "Defines an identity in central factorial numbers (2k-1)! T(2n, 2k) = \\frac{1}{2k} \\sum_{j=0}^{2k} \\binom{2k}{j} (-1)^{j} (k-j)^{2n}"
 
 Begin["`Private`"]
 CentralFactorialNumber1[n_, n_] = CentralFactorialNumber1[n_, 1] = 1;
@@ -12,6 +13,7 @@ CentralFactorialNumber1[n_, k_] := CentralFactorialNumber1[n-1, k-1] + k^2 Centr
 
 CFNIdentity1[n_, k_] := (1/k) * Sum[ (-1)^j * Binomial[2k, j] * (k-j)^(2n), {j, 0, k}];
 CFNIdentity2[n_, k_] := (1/k) * Sum[ (-1)^(k-j) * Binomial[2k, k-j] * j^(2n), {j, 0, k}];
+CFNIdentity3[n_, k_] := (1/(2k)) * Sum[ (-1)^j * Binomial[2k, j] * (k-j)^(2n), {j, 0, 2k}];
 End[ ]
 
 EndPackage[ ]
