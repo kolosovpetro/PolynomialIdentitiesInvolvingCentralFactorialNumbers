fb1306702bb6133701407f857b31b3f8db1c1d92
kolosovpetro <kolosovp94@gmail.com>
2023-07-22 13:42:04 +0200

    odd power identity validation

--- mathematica/PolynomialIdentitiesInvolvingCentralFactorialNumbers.m
+++ mathematica/PolynomialIdentitiesInvolvingCentralFactorialNumbers.m
@@ -10,6 +10,9 @@ CFNIdentity3::usage= "Defines an identity in central factorial numbers (2k-1)! T
 OddPowerIdentity1::usage= "Defines an odd power identity n^{2m-1} = \\sum_{k=1}^{m} \\mathtt{CFNIdentity1} (m,k) \\binom{n+k-1}{2k-1}"
 OddPowerIdentity11::usage= "Defines an odd power identity n^{2m-1} = \\sum_{k=1}^{m} \\sum_{j=0}^{k} \\frac{(-1)^j}{k} \\binom{2k}{j} \\binom{n+k-1}{2k-1} (k-j)^{2m}"
 OddPowerIdentity12::usage= "Defines an odd power identity n^{2m-1} = \\sum_{k=1}^{m} \\sum_{j=0}^{k} \\frac{(-1)^{j}}{k} \\frac{2k}{n+k} \\binom{n+k}{2k} \\binom{2k}{j} (k-j)^{2m}"
+OddPowerIdentity13::usage= "Defines an odd power identity n^{2m-1} = 2\\sum_{k=1}^{m} \\sum_{j=0}^{k} \\frac{(-1)^{j}}{n+k} \\binom{n+k}{j} \\binom{n+k-j}{2k-j} (k-j)^{2m}"
+OddPowerIdentity14::usage= "Defines an odd power identity n^{2m-1} = 2\\sum_{k=1}^{m} \\sum_{j=0}^{k} \\frac{(-1)^{j}}{n+k} \\binom{n+k}{n+k-j} \\binom{n+k-j}{2k-j} (k-j)^{2m}"
+OddPowerIdentity15::usage= "Defines an odd power identity "
 OddPowerIdentity2::usage= "Defines an odd power identity n^{2m-1} = \\sum_{k=1}^{m} \\mathtt{CFNIdentity2} (m,k) \\binom{n+k-1}{2k-1}"
 OddPowerIdentity3::usage= "Defines an odd power identity n^{2m-1} = \\sum_{k=1}^{m} \\mathtt{CFNIdentity3} (m,k) \\binom{n+k-1}{2k-1}"
 OddPowerIdentity21::usage= "Defines an odd power identity n^{2m-1} = \\sum_{k=1}^{m} \\sum_{j=0}^{k} \\frac{(-1)^{k-j}}{k} \\binom{n+k-1}{2k-1} \\binom{2k}{k-j} j^{2m}"
@@ -31,6 +34,9 @@ CFNIdentity3[n_, k_] := (1/(2k)) * Sum[ (-1)^j * Binomial[2k, j] * (k-j)^(2n), {
 OddPowerIdentity1[n_, m_] := Sum[CFNIdentity1[m, k] * Binomial[n+k-1, 2k-1], {k, 1, m}];
 OddPowerIdentity11[n_, m_] := Sum[ Sum[ ((-1)^j)/k * Binomial[2k, j] * Binomial[n+k-1, 2k-1] * (k-j)^(2m), {j, 0, k}], {k, 1, m}];
 OddPowerIdentity12[n_, m_] := Sum[ Sum[ ((-1)^j)/k * (2k)/(n+k) * Binomial[n+k, 2k] * Binomial[2k, j] * (k-j)^(2m), {j, 0, k}], {k, 1, m}];
+OddPowerIdentity13[n_, m_] := 2 * Sum[ Sum[ ((-1)^j)/(n+k) * Binomial[n+k, j] * Binomial[n+k-j, 2k-j] * (k-j)^(2m), {j, 0, k}], {k, 1, m}];
+OddPowerIdentity14[n_, m_] := 2 * Sum[ Sum[ ((-1)^j)/(n+k) * Binomial[n+k, n+k-j] * Binomial[n+k-j, 2k-j] * (k-j)^(2m), {j, 0, k}], {k, 1, m}];
+OddPowerIdentity15[n_, m_] := 2 * Sum[ Sum[ ((-1)^j)/(n+k) * Binomial[n+k, 2k-j] * Binomial[n-k+j, n-k] * (k-j)^(2m), {j, 0, k}], {k, 1, m}];
 OddPowerIdentity2[n_, m_] := Sum[CFNIdentity2[m, k] * Binomial[n+k-1, 2k-1], {k, 1, m}];
 OddPowerIdentity3[n_, m_] := Sum[CFNIdentity3[m, k] * Binomial[n+k-1, 2k-1], {k, 1, m}];
 OddPowerIdentity21[n_, m_] := Sum[ Sum[ (-1)^(k-j) / k * Binomial[n+k-1, 2k-1] * Binomial[2k, k-j]* j^(2m), {j, 0, k}], {k, 1, m}];
